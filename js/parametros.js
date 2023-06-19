897ab62abf3b013c73f719ff26cde9bf705f1ee5
Maximiliano Chavez <104655710+Maxigamble@users.noreply.github.com>
2023-06-20 00:23:50 +0200

    Obtengo la dificultad del home.Genero, colores dinamicamente y agrego inputs

--- js/parametros.js
+++ js/parametros.js
@@ -1,2 +1,32 @@
 let dificultad = JSON.parse(sessionStorage.sesion).dificultad;
 console.log(dificultad);
+let arrayColores = [];
+
+const generarPickers = (cantidad) => {
+  let colorPick = document.getElementById("containerPick");
+
+  for (let i = 1; i <= cantidad; i++) {
+    let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    arrayColores.push(color);
+    let divcolor = `<div class="col d-flex">
+    <input id="pick${i}" class="input-color" type="color" value="${color}" />
+    </div>`;
+
+    colorPick.innerHTML += divcolor;
+  }
+};
+
+switch (dificultad) {
+  case "Fácil":
+    generarPickers(4);
+    break;
+
+  case "Intermedio":
+    generarPickers(5);
+    break;
+
+  default:
+    "Dificil";
+    generarPickers(6);
+    break;
+}
