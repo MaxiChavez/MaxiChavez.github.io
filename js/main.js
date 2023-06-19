4dd817faec0622ac96b31fba667b5747a4773c7d
Maximiliano Chavez <104655710+Maxigamble@users.noreply.github.com>
2023-06-19 23:19:40 +0200

    Se crea estructura del proyecto, dos vistas y su correspondiente css

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value;
+
+  let dificult = document.getElementById("select-dificultad");
+  let value = dificult.value;
+  let text = dificult.options[dificult.selectedIndex].text;
+  console.log(text);
+  let nombreDificultad = {
+    nombre: name,
+    dificultad: text,
+  };
+  sessionStorage.setItem("sesion", JSON.stringify(nombreDificultad));
+
+  window.location.href = "../pages/parametros.html";
+});
+
+// var selectedOption = this.options[select.selectedIndex];
+//     console.log(selectedOption.value + ': ' + selectedOption.text)
+
+//     var e = document.getElementById("ddlViewBy");
+// var value = e.value;
+// var text = e.options[e.selectedIndex].text;
