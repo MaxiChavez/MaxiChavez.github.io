4e28a6ed45f8ef7eea01b115f902c31c49036252
Maximiliano Chavez <104655710+Maxigamble@users.noreply.github.com>
2023-07-05 19:00:39 +0200

    Se soluciona error de seleccion de colores,funcion RGB to HEX

--- js/parametros.js
+++ js/parametros.js
@@ -5,15 +5,22 @@ let arrayColores = [];
 const generarPickers = (cantidad) => {
   let colorPick = document.getElementById("containerPick");
 
-  for (let i = 1; i <= cantidad; i++) {
-    let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    arrayColores.push(color);
+  for (let i = 0; i < cantidad; i++) {
+    let color = generateRandomColor();
     let divcolor = `<div class="col d-flex">
-    <input id="pick${i}" class="input-color" type="color" value="${color}" />
+    <input id="pick${i}" data-numero="${i}"class="input-color" type="color" value="${color}" onchange ="updateColor(this)" />
     </div>`;
-
+    // let divcolor = `<div class="col d-flex">
+    // //     <input id="pick${i}" class="input-color" type="color" value="${color}" />
+    // //     </div>`;
+    arrayColores[i] = color;
     colorPick.innerHTML += divcolor;
   }
+};
+
+const updateColor = (obj) => {
+  document.getElementById(obj.id).defaultValue = `${obj.value}`;
+  arrayColores[obj.dataset.numero] = `${obj.value}`;
 };
 
 switch (dificultad) {
@@ -37,3 +44,20 @@ const jugar = () => {
   sessionStorage.setItem("sesion", JSON.stringify(dataSesion));
   window.location.href = "../pages/mastermind.html";
 };
+
+function generateRandomColor() {
+  // Generar valores aleatorios para los componentes RGB
+  var r = Math.floor(Math.random() * 256); // Valores entre 0 y 255
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+
+  // Convertir los componentes RGB a formato hexadecimal
+  var hexR = r.toString(16).padStart(2, "0"); // Convertir a hexadecimal y rellenar con ceros si es necesario
+  var hexG = g.toString(16).padStart(2, "0");
+  var hexB = b.toString(16).padStart(2, "0");
+
+  // Combinar los componentes en un único valor hexadecimal
+  var hexColor = "#" + hexR + hexG + hexB;
+
+  return hexColor;
+}
